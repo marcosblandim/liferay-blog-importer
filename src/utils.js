57f760a545b01a7f84cc089b1c72dabaa1d23987
marcos.blandim <marcos.blandim@seatecnologia.com.br>
2021-12-13 18:37:51 -0300

    fix: utils format

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,5 @@
 // TODO: implement suport for empty curly brackets (?)
-export default function format(string, args) {
+export function format(string, ...args) {
     return string.replace(/{(\d+)}/g, function (match, number) {
         return typeof args[number] != 'undefined' ? args[number] : match;
     });
